7e18f7662c61e0dd0e8814c719439d3bcb7899b0
avdheshkimothi <257133074+avdheshkimothi@users.noreply.github.com>
2026-06-04 17:11:44 +0530

    Add sum of digits program

---
+++ sum_of_digits_of_given_number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+     cout<<"ENTER THE NUMBER";
+     cin>>number;
+     int sum_of_num=0;
+     int y;
+     // if number will be 0 it will print sum=0 at console
+     if(number==0){
+        cout<<"0";
+        exit(1);
+        }
+     if(number<0)
+     {
+     number=-number;
+     }
+     // logic of finding sum 
+    while(number!=0){
+        y=number%10;
+        sum_of_num+=y;
+        number/=10;
+
+        
+
+    }
+    cout<<sum_of_num;
+    return 0;
+}
